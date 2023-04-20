a8d24592a346e5cc6bdc3ecf374115357fbecb38
Oleg Telegin <teleginos@mail.ru>
2023-04-20 22:48:16 +0300

    add solution task2

---
+++ Task№2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2) Console.Write($"max = {number1}");
+else {Console.Write($"max = {number2}");}
