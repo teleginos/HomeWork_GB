e3a92b28f41c100ec58af4cf9b6927d7f87e95f6
Oleg Telegin <teleginos@mail.ru>
2023-04-20 22:59:01 +0300

    add solution task4

---
+++ Task№4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2 & number1 > number3) Console.Write($"Максималльное из чисел = {number1}");
+if (number2 > number1 & number2 > number3) Console.Write($"Максималльное из чисел = {number2}");
+if (number3 > number1 & number3 > number2) Console.Write($"Максималльное из чисел = {number3}");
+
